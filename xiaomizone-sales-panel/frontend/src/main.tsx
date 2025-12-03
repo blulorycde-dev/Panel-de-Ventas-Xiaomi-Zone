bdbff823088a655d8db70cc5b2baab9ba35af01a
blulorycde-dev <blulorycde@gmail.com>
2025-12-03 14:41:55 -0300

    Update main.tsx

--- xiaomizone-sales-panel/frontend/src/main.tsx
+++ xiaomizone-sales-panel/frontend/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { SalesPage } from "./pages/SalesPage";
 import "./styles/globals.css";
 import "./styles/theme.css";
+import { CartProvider } from "./hooks/useCart";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SalesPage />
+    <CartProvider>
+      <SalesPage />
+    </CartProvider>
   </React.StrictMode>
 );
