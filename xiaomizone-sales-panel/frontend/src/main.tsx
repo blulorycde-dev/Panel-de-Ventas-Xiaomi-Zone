25b7bc9bf39a75440b513dc4b1fb450b187fbf6b
blulorycde-dev <blulorycde@gmail.com>
2025-12-03 12:15:52 -0300

    Update main.tsx

---
+++ xiaomizone-sales-panel/frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SalesPage } from "./pages/SalesPage";
+import "./styles/globals.css";
+import "./styles/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <SalesPage />
+  </React.StrictMode>
+);
