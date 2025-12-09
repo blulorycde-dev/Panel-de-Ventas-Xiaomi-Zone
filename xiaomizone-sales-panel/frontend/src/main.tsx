586abfa095c4677ada7c0b02ede09858739e1fee
blulorycde-dev <blulorycde@gmail.com>
2025-12-09 07:42:54 -0300

    Update main.tsx

--- xiaomizone-sales-panel/frontend/src/main.tsx
+++ xiaomizone-sales-panel/frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './hooks/useCart';
+// frontend/src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./hooks/useCart";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
