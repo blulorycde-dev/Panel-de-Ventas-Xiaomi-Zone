25948d89b9e3d138fe0755ab88be94f15c8a63ea
blulorycde-dev <blulorycde@gmail.com>
2025-12-11 14:03:53 -0300

    Create App.tsx

---
+++ xiaomizone-sales-panel/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SalesPage } from "./pages/sales";
+
+const App: React.FC = () => {
+  return (
+    <div className="app-root">
+      <SalesPage />
+    </div>
+  );
+};
+
+export default App;
