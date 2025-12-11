3c98cfc2b432adc4909b4d6b1b08923c0de4584f
blulorycde-dev <blulorycde@gmail.com>
2025-12-11 14:23:39 -0300

    Update main.tsx

--- xiaomizone-sales-panel/frontend/src/main.tsx
+++ xiaomizone-sales-panel/frontend/src/main.tsx
@@ -1,9 +1,8 @@
-// frontend/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./hooks/useCart";
-import { SalesPage } from "./pages/SalesPage"; // ← OJO: carpeta pages + archivo SalesPage.tsx
+import { SalesPage } from "./pages/SalesPage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
